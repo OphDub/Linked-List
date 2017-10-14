cceae1cae515fc4a4971e879919c4d57694bab19
Ophus Wong <ophus.wong@gmail.com>
2017-10-14 17:14:59 -0600

    Add functionality for delete button

--- scripts.js
+++ scripts.js
@@ -9,9 +9,10 @@ var displayWrapper = document.querySelector('.display-wrapper');
 function displayBookmark() {
 	bookmarkCard = document.createElement('div');
 	bookmarkCard.className = 'bookmark';
-	bookmarkCard.innerHTML = '<h3>'+urlTitle+'</h3><a class="user-supplied-link" href=""> '+url+'</a><div class="read-delete"><button class="left" id="read-button" href="">Read</button><button class="right" href="">Delete</button></div>';
+	bookmarkCard.innerHTML = '<h3>'+urlTitle+'</h3><a class="user-supplied-link" href=""> '+url+'</a><div class="read-delete"><button class="left" id="read-button">Read</button><button class="right" id="delete-button">Delete</button></div>';
 	displayWrapper.appendChild(bookmarkCard);
 	document.getElementById('read-button').addEventListener('click', markAsRead);
+	document.getElementById('delete-button').addEventListener('click', deleteCard);
 };
 
 function markAsRead() {
@@ -21,6 +22,11 @@ function markAsRead() {
 	bookmarkElement.classList.toggle('bookmark-read');
 	urlStyle.classList.toggle('user-supplied-link-read');
 	readButton.classList.toggle('left-read');
+}
+
+function deleteCard() {
+	console.log('Delete attempt');
+	this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
 }
 
 //Event Listeners
@@ -36,3 +42,4 @@ document.querySelector('.url-list').addEventListener('click', function(event) {
 	console.log('Class change');
 	}
 });
+
