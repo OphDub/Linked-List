874718c94b65a4dbfac845962b53bbd869ce40ac
Ophus Wong <ophus.wong@gmail.com>
2017-10-14 13:23:01 -0600

    Add semi-functionality for dynamic bookmark

--- scripts.js
+++ scripts.js
@@ -2,8 +2,16 @@
 // var enterButton = document.querySelector('.enter-button');
 var url = document.getElementById('url');
 var urlTitle = document.getElementById('title');
-//Functions
+var bookmarkCard
+var displayWrapper = document.querySelector('.display-wrapper');
 
+//Functions
+function displayBookmark() {
+	//bookmarkCard = '<div class="bookmark"><h3>The Website Title</h3><a class="user-supplied-link" href=""> '+url+'</a><div class="read-delete"><a class="left" href="">Read</a><a class="right" href="">Delete</a></div></div>';
+	displayWrapper.innerHTML = url;
+	displayWrapper.innerHTML = urlTitle;
+	console.log();
+};
 //Button
 
 //Event Listeners
@@ -11,5 +19,6 @@ var urlTitle = document.getElementById('title');
 document.querySelector('.enter-button').addEventListener('click', function() {
 	url = url.value;
   urlTitle = urlTitle.value;
-	console.log(url, urlTitle)
+	console.log(url, urlTitle);
+	displayBookmark();
 }) 
