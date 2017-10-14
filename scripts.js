21a71b8f8224a21f8aede6d5d40a432ebd45d6a8
parker lindley <fugazi8@gmail.com>
2017-10-14 16:29:56 -0600

    Add .read sytle on click, functioning beginning

--- scripts.js
+++ scripts.js
@@ -4,28 +4,35 @@ var url = document.getElementById('url');
 var urlTitle = document.getElementById('title');
 var bookmarkCard
 var displayWrapper = document.querySelector('.display-wrapper');
-var readButton 
+
 
 //Functions
 function displayBookmark() {
 	bookmarkCard = document.createElement('div');
 	bookmarkCard.className = 'bookmark';
 	bookmarkCard.innerHTML = '<h3>'+urlTitle+'</h3><a class="user-supplied-link" href=""> '+url+'</a><div class="read-delete"><button class="left" id="read-button" href="">Read</button><button class="right" href="">Delete</button></div>';
-	readButton = document.getElementById('read-button');
-	var bookmarkElement = document.querySelector('.bookmark');
+	
 	displayWrapper.appendChild(bookmarkCard);
-};
 
-function markAsRead() {
-	bookmarkElement.className += 'bookmark-read';
-	url.className += 'user-supplied-link-read';
-	readButton.className += 'left-read';
-	readButton.innerHTML = 'It worked';
+	document.getElementById('read-button').addEventListener('click', markAsRead);
+
+
+
+	function markAsRead() {
+	var bookmarkElement = document.querySelector('.bookmark');
+	var readButton = document.getElementById('read-button');
+	var urlStyle = document.getElementById('url');
+	bookmarkElement.classList.toggle('bookmark-read');
+	urlStyle.classList.toggle('user-supplied-link-read');
+	readButton.classList.toggle('left-read');
 }
 
-//Button
+};
 
-//Event Listeners
+
+
+
+
 
 document.querySelector('.enter-button').addEventListener('click', function() {
 	url = url.value;
