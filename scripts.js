591da0b8e81de9d2e53379683a5be81d34fd4fcf
parker lindley <fugazi8@gmail.com>
2017-10-15 12:46:21 -0600

    Phase 1 complete, remove unused trial functions

--- scripts.js
+++ scripts.js
@@ -1,30 +1,17 @@
 //Global Variables
 var displayWrapper = document.querySelector('.display-wrapper');
 
-
 //Functions
 
 //DISPLAY FUNCTION
 function displayBookmark() {
 	var url = document.getElementById('url').value;
 	var urlTitle = document.getElementById('title').value;
-	var bookmarkCard = document.createElement('li');
+	var bookmarkCard = document.createElement('div');
 	bookmarkCard.className = 'bookmark';
 	bookmarkCard.innerHTML = '<h3>'+urlTitle+'</h3><a class="user-supplied-link" href=""> '+url+'</a><div class="read-delete"><button class="left" id="read-button">Read</button><button class="right" id="delete-button">Delete</button></div>';
 	displayWrapper.appendChild(bookmarkCard);
-	// readDeleteButtons();
-	// 
 };
-
-//ADD .READ CLASS ON CLICK
-// function markAsRead() {
-// 	var bookmarkElement = document.querySelector('.bookmark');
-// 	var readButton = document.getElementById('read-button');
-// 	var urlStyle = document.getElementById('url');
-// 	bookmarkElement.classList.toggle('bookmark-read');
-// 	urlStyle.classList.toggle('user-supplied-link-read');
-// 	readButton.classList.toggle('left-read');
-// }
 
 document.querySelector('.enter-button').addEventListener('click', function() {
 	displayBookmark();
@@ -41,42 +28,12 @@ function removeCard(e) {
 }
 
 document.querySelector('.display-wrapper').addEventListener('click', removeCard);
-// document.querySelector('.display-wrapper').addEventListener('click', markAsRead);
 
 $("#right-container").on('click', '#read-button', function() {
-	console.log("hi")
-	console.log($(this).text());
 	$(this).toggleClass('left-read')
-	console.log($(this).parents()[1]);
 	$(this).closest('.bookmark').toggleClass('bookmark-read');
 	$(this).closest('a').toggleClass('user-supplied-link-read');
-	
-	
-
 })
 
 
 
-// function markAsRead2(e) {
-// 	var bookmarkElement = document.querySelector('.bookmark');
-// 	var readButton = document.getElementById('read-button');
-// 	var urlStyle = document.getElementById('url');
-// 	var readTarget = e.target
-// 		if (readTarget.className !== 'left') {
-// 			return;
-// 		} 
-// 		if (bookmarkElement.classList.contains('bookmark-read')) {
-// 					bookmarkElement.classList.remove('bookmark-read');
-// 		} else {
-// 					bookmarkElement.classList.add('bookmark-read');
-// 		}
-
-// 		// urlStyle.classList.toggle('user-supplied-link-read');
-// 		// readButton.classList.toggle('left-read');
-// 		console.log("read clicked");
-// }
-// 
-// 	var container = document.querySelectorAll('.display-wrapper');
-//for (var i =0; i <container.length; i++) {
-//	document.querySelector('.left')[i].addEventListener('click', markAsRead2);
-//}
