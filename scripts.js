c0184f8fd802f16f4b062c31415a36e32221e4f4
parker lindley <fugazi8@gmail.com>
2017-10-14 22:23:14 -0600

    Add read class toggle, only works on first dynamic add, all delete buttons work

--- scripts.js
+++ scripts.js
@@ -8,11 +8,13 @@ var displayWrapper = document.querySelector('.display-wrapper');
 function displayBookmark() {
 	var url = document.getElementById('url').value;
 	var urlTitle = document.getElementById('title').value;
-	var bookmarkCard = document.createElement('div');
+	var bookmarkCard = document.createElement('li');
+
 	bookmarkCard.className = 'bookmark';
 	bookmarkCard.innerHTML = '<h3>'+urlTitle+'</h3><a class="user-supplied-link" href=""> '+url+'</a><div class="read-delete"><button class="left" id="read-button">Read</button><button class="right" id="delete-button">Delete</button></div>';
 	displayWrapper.appendChild(bookmarkCard);
-	readDeleteButtons();
+	// readDeleteButtons();
+	// 
 };
 
 //ADD .READ CLASS ON CLICK
@@ -25,32 +27,46 @@ function markAsRead() {
 	readButton.classList.toggle('left-read');
 }
 
-//DELETE CARD
-// function deleteCard() {
-// 	console.log('Delete attempt');
-// 	this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
-// }
-
-//DISPLAY BOOKMARK LISTENER
 document.querySelector('.enter-button').addEventListener('click', function() {
 	displayBookmark();
 }) 
-
-function readDeleteButtons () {
-	document.getElementById('read-button').addEventListener('click', markAsRead);
-	// document.getElementById('delete-button').addEventListener('click', deleteCard);
-}
 
 function removeCard(e) {
 	var target = e.target
 	while (target.className !== 'display-wrapper') {
 		if(target.className === 'right') {
 			return target.parentNode.parentNode.parentNode.removeChild(target.parentNode.parentNode);
-			console.log("bye")
 		}
-		console.log("hi")
 		 target = target.parentNode.parentNode;
 	}
 }
 
 document.querySelector('.display-wrapper').addEventListener('click', removeCard);
+document.querySelector('.display-wrapper').addEventListener('click', markAsRead);
+
+
+// function markAsRead2(e) {
+// 	var bookmarkElement = document.querySelector('.bookmark');
+// 	var readButton = document.getElementById('read-button');
+// 	var urlStyle = document.getElementById('url');
+// 	var readTarget = e.target
+// 		if (readTarget.className !== 'left') {
+// 			return;
+// 		} 
+// 		if (bookmarkElement.classList.contains('bookmark-read')) {
+// 					bookmarkElement.classList.remove('bookmark-read');
+// 		} else {
+// 					bookmarkElement.classList.add('bookmark-read');
+// 		}
+
+// 		// urlStyle.classList.toggle('user-supplied-link-read');
+// 		// readButton.classList.toggle('left-read');
+// 		console.log("read clicked");
+// }
+// 
+// 	var container = document.querySelectorAll('.display-wrapper');
+//for (var i =0; i <container.length; i++) {
+//	document.querySelector('.left')[i].addEventListener('click', markAsRead2);
+//}
+
+//DISPLAY BOOKMARK LISTENER
